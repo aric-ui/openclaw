53d3440d4e6d12b9442fa4d2f5ed859cc60f8294
aric-ui <aric@818shuttersandshades.com>
2026-02-11 12:49:03 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ~/.openclaw
 echo '{"channels":{"telegram":{"enabled":true,"dmPolicy":"open","allowFrom":["*"]}}}' > ~/.openclaw/openclaw.json
-node dist/index.js gateway
+node dist/index.js gateway --allow-unconfigured
